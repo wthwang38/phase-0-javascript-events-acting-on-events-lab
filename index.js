95cd90252795a5cebaee48741644af129bee2499
William Hwang <willhwang90@gmail.com>
2022-05-03 11:18:14 -0400

    commit tree df07f506b8418b3990942eb5dc4a6e9536dc8162

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+// key: = "ArrowLeft"
+// key: "ArrowRight"
+const dodger = document.getElementById('dodger');
+
+document.addEventListener("keydown",(e)=> {
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    } else if (e.key === "ArrowUp"){
+        console.log(e);
+    } else if (e.key === "ArrowDown"){
+        console.log(e);
+    }
+});
+
+
+const moveDodgerLeft = ()=>{
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0){
+        dodger.style.left = `${left - 10}px`;
+    }
+
+}
+const moveDodgerRight = ()=>{
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 355){
+        dodger.style.left = `${left + 10}px`;
+    }
+}
+const moveDodgerUp = (e) => console.log(e);
+const moveDodgerDown = (e) => console.log(e);
+
+/*if (e.key === "ArrowLeft"){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 10}px`;
+} else if (e.key === "ArrowRight"){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left + 10}px`;
+}
+*/
